709ded6d13df1e4ba83f45d0758775e3691081b4
Yoann Poupart <yoann.poupart@ens-lyon.org>
2024-07-13 15:49:28 +0200

    act dim

--- scripts/sae_training/run_contrastive.py
+++ scripts/sae_training/run_contrastive.py
@@ -85,8 +85,10 @@ def make_contrastive_run(
         val_ds,
         batch_size=run_config.batch_size,
     )
-
-    act_dim = train_ds[0]["root_act"].shape[0] * 2
+    if streaming:
+        act_dim = next(iter(train_ds)).shape[0] * 2
+    else:
+        act_dim = train_ds[0]["root_act"].shape[0] * 2
     dict_size = run_config.dict_size or run_config.dict_size_scale * act_dim
     sae = trainSAE(
         train_dataloader,
--- scripts/sae_training/run_regular.py
+++ scripts/sae_training/run_regular.py
@@ -83,7 +83,10 @@ def make_regular_run(
         batch_size=run_config.batch_size,
     )
 
-    act_dim = train_ds[0]["root_act"].shape[0] * 2
+    if streaming:
+        act_dim = next(iter(train_ds)).shape[0]
+    else:
+        act_dim = train_ds[0]["root_act"].shape[0]
     dict_size = run_config.dict_size or run_config.dict_size_scale * act_dim
     sae = trainSAE(
         train_dataloader,
